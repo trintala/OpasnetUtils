8a7b18453a4934caace6f9189504d0fc3ed0c8b3
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-07-05 10:37:33 +0000

    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@44 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/CheckMarginals.r
+++ trunk/R/CheckMarginals.r
@@ -3,7 +3,7 @@
 # Returns an ovariable with a marginal devised from the data and upstream variable marginals. 
 # Marginal values for data should be stored into the database somehow
 
-CheckMarginals <- function(variable) {
+CheckMarginals <- function(variable) { # extra inputs necessary for ops functionality
 	varmar <- colnames(variable@data)[
 		!grepl(paste("^", variable@name, "", sep=""), colnames(variable@data))&
 		!colnames(variable@data) %in% c("Result", "Unit")
@@ -13,8 +13,8 @@ CheckMarginals <- function(variable) {
 	# by EvalOutput so it should be in the initial list by default. 
 	novarmar <- colnames(variable@data)[!colnames(variable@data) %in% varmar]
 	for (i in as.character(variable@dependencies$Name)){
-		varmar <- unique(varmar, colnames(get(i)@output)[get(i)@marginal])
-		novarmar <- unique(novarmar, colnames(get(i)@output)[!get(i)@marginal])
+		varmar <- unique(c(varmar, colnames(get(i)@output)[get(i)@marginal]))
+		novarmar <- unique(c(novarmar, colnames(get(i)@output)[!get(i)@marginal]))
 	}
 	varmar <- varmar[!varmar %in% novarmar]
 	variable@marginal <- colnames(variable@output) %in% varmar
