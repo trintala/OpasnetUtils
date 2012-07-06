821f92ecfac142232066cdce6549aebfa54590a4
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-07-06 11:19:15 +0000

    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@49 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/ComputeDependencies.r
+++ R/ComputeDependencies.r
@@ -2,10 +2,10 @@
 # upstream variables. Typically seen on the first line of ovariable formula code. 
 # '...' can be used for input substitution, na.rm, number of iterations (N) and others
 
-ComputeDependencies <- function(dependencies, ...) { 
-	Fetch2(dependencies)
+ComputeDependencies <- function(dependencies, forceEval = FALSE, ...) { 
+	Fetch2(dependencies, ...)
 	for (i in as.character(dependencies$Name)) {
-		assign(i, EvalOutput(get(i), ...), envir = .GlobalEnv)
+		if (nrow(get(i)@output) == 0 | forceEval) assign(i, EvalOutput(get(i), ...), envir = .GlobalEnv)
 		assign(i, CheckMarginals(get(i), ...), envir = .GlobalEnv)
 		assign(i, CheckInput(get(i), ...), envir = .GlobalEnv)
 	}
