ac84c0919886e41366ad1f5ea766863e429cdebe
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-07-12 08:05:38 +0000

    Bug splat
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@62 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/CheckInput.r
+++ R/CheckInput.r
@@ -47,7 +47,7 @@ CheckInput <- function(variable, substitute = FALSE, ...) { # ... e.g for na.rm
 		temp <- merge(temp, inputvar@output)
 		variable@output <- melt(
 			temp, 
-			measure.vars = c(levels(variable@output[,paste(variable@name, "Source", sep = "")]), "Input"), 
+			measure.vars = c(j, "Input"), 
 			variable.name = paste(variable@name, "Source", sep = ""), 
 			value.name = paste(variable@name, "Result", sep = ""), 
 			...
