de6acd5d94fcfcec8bbbd851851ad3082f6fc95b
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-07-17 10:40:46 +0000

    Bug splat
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@67 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/ComputeDependencies.r
+++ R/ComputeDependencies.r
@@ -3,10 +3,12 @@
 # '...' can be used for input substitution, na.rm, number of iterations (N) and others
 
 ComputeDependencies <- function(dependencies, forceEval = FALSE, ...) { 
+	cat("\n")
 	Fetch2(dependencies, ...)
 	for (i in as.character(dependencies$Name)) {
 		if (nrow(get(i)@output) == 0 | forceEval) assign(i, EvalOutput(get(i), ...), envir = .GlobalEnv)
 		assign(i, CheckMarginals(get(i), ...), envir = .GlobalEnv)
 		assign(i, CheckInput(get(i), ...), envir = .GlobalEnv)
 	}
+	cat("\n")
 }
