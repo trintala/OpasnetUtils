2fb59749fef3094a18d6f71811eeb383e335aff6
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-08-14 07:14:37 +0000

    CollapseMarginal function added
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@93 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/CollapseMarginal.r
+++ trunk/R/CollapseMarginal.r
@@ -5,36 +5,22 @@
 # Also loses all non-marginal columns except the relevant Result
 #####################################
 
-CollapseMarginal <- function(variable, cols, method = "Sample", ...) { # ... prob = <vector> for sampling weights
-	out <- variable@output
-	func <- CollapsePreset(method) # get the selected function
-	marginals <- colnames(out)[variable@marginal]
-	out <- tapply(
-		out[[paste(variable@name, "Result", sep = "")]], 
-		out[[marginals[!marginals %in% cols]]],
-		func,
-		...
-	)
-	out <- as.data.frame(as.table(out))
-	colnames(out)[colnames(out) == "Freq"] <- paste(variable@name, "Result", sep = "")
-	#for(i in colnames(out)[colnames(out) %in% cols])
-	variable@output <- out
-	variable@marginal <- colnames(out) %in% marginals # Marginals can be easily corrected here disrequiring CheckMarginals
-	return(variable)
-}
-
-
-
-CollapsePreset <- function(name) {
-	if (name == "Sum") return(function(x, ...) sum(x)) # ... needs to be allowed
-	if (name == "Mean") return(function(x, ...) mean(x))
-	if (name == "Sample") return(function(x, ...) sample(x, size = 1, ...))
-}
-
-CollapseTableParser <- function(ctable) {
-	for (i in rownames(ctable)) {
-		
+CollapseTableParser <- function(CTable){ # DTable is a data.frame
+	for (i in unique(as.character(CTable$Variable))) {
+		temp <- CTable[CTable$Variable == i,] # c("Decision", "Option")]
+		cols <- temp[["Index"]]
+		probs <- strsplit(temp[["Probs"]], ",") 
+		out <- list(cols, probs)
+		assign(paste("Col", i), out, envir = .GlobalEnv)
 	}
+}
+
+CheckCollapse <- function(variable, indent = 0, ...) {
+	if (exists(paste("Col", variable@name, sep = ""))) {
+		Col <- get(paste("Col", variable@name, sep = ""))
+		variable <- CollapseMarginal(variable, Col$cols, Col$probs, ...)
+	}
+	return(variable)
 }
 
 CollapseMarginal <- function(variable, cols, probs = NULL, ...) { # cols is a character vector, while probs is a list
@@ -50,15 +36,28 @@ CollapseMarginal <- function(variable, cols, probs = NULL, ...) { # cols is a ch
 			a <- a + 1
 			b <- probs[[a]]
 			if(is.null(b)) b <- rep(1, N)
-			selection <- tapply( # For each iteration, sample from available locations. All locations should exist for
-				out[[i]], # all iterations, but tapply is the most efficient way of doing the sampling and it adds
-				out[["Iter"]], # extra robustness.
-				sample,
-				size = 1,
-				prob = b
+			#selection <- tapply( # For each iteration, sample from available locations. All locations should exist for
+			#	out[[i]], # all iterations, but tapply is the most efficient way of doing the sampling and it adds
+			#	out[["Iter"]], # extra robustness. Not going to work with given probabilities, since the input would 
+			#	sample, # vary in length depending on the number of other marginals etc.
+			#	size = 1, 
+			#	prob = b
+			#)
+			#temp <- rownames(selection)
+			#selection <- data.frame(selection, Iter = temp)
+			selection <- data.frame(
+				sample(
+					unique(
+						as.character(
+							out[[i]]
+						)
+					), 
+					size = N, 
+					replace = TRUE, 
+					prob = b
+				), 
+				Iter = 1:N
 			)
-			temp <- rownames(selection)
-			selection <- data.frame(selection, Iter = temp)
 			colnames(selection)[1] <- i
 			out <- merge(out, selection)
 		}
