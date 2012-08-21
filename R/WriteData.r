398d7c53382b0658bf241a684bab2c9608ac810e
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-08-21 10:21:54 +0000

    Bug splat
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@99 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/WriteData.r
+++ R/WriteData.r
@@ -1,25 +1,25 @@
-$header = array(
-	'object'=>array(
-		'name' => "Ahaaa",
-		'ident' => "testi.kaavii",
-		'subset_name' => 'Kaavi',
-		'type' => "variable",
-		'page' => 123578,
-		'wiki_id' =>1
-	),
-	'act'=>array(
-		'unit' => "kg/cm3",
-		'who' => "Alberto",
-		'samples' => 1,
-		'comments' => "Tämä on kommentti tähän actiin",
-		'language' => 'eng'
-	),
-	'indices'=>array(
-		1=>array('type'=>'entity','name'=>'Indeksi ykkönen','page'=>666,'wiki_id'=>1,'order_index'=>1,'hidden'=>0,’unit’=>’brand’),
-		2=>array('type'=>'number','name'=>'Indeksi kakkonen','page'=>777,'wiki_id'=>2,'order_index'=>2,'hidden'=>0,’unit’=>’cm’),
-		3=>array('type'=>'time','name'=>'Indeksi kolmonen','page'=>888,'wiki_id'=>1,'order_index'=>3,'hidden'=>1),
-	)                
-);
+#$header = array(
+#	'object'=>array(
+#		'name' => "Ahaaa",
+#		'ident' => "testi.kaavii",
+#		'subset_name' => 'Kaavi',
+#		'type' => "variable",
+#		'page' => 123578,
+#		'wiki_id' =>1
+#	),
+#	'act'=>array(
+#		'unit' => "kg/cm3",
+#		'who' => "Alberto",
+#		'samples' => 1,
+#		'comments' => "Tämä on kommentti tähän actiin",
+#		'language' => 'eng'
+#	),
+#	'indices'=>array(
+#		1=>array('type'=>'entity','name'=>'Indeksi ykkönen','page'=>666,'wiki_id'=>1,'order_index'=>1,'hidden'=>0,’unit’=>’brand’),
+#		2=>array('type'=>'number','name'=>'Indeksi kakkonen','page'=>777,'wiki_id'=>2,'order_index'=>2,'hidden'=>0,’unit’=>’cm’),
+#		3=>array('type'=>'time','name'=>'Indeksi kolmonen','page'=>888,'wiki_id'=>1,'order_index'=>3,'hidden'=>1),
+#	)                
+#);
 
  
 
@@ -27,25 +27,25 @@ $header = array(
 
  
 
-$data['indices']=array(
-	1=>array('name'=>'Indeksi ykkönen'),
-	2=>array('name'=>'Indeksi kakkonen'),
-	3=>array('name'=>'Indeksi kolmonen')
-);
+#$data['indices']=array(
+#	1=>array('name'=>'Indeksi ykkönen'),
+#	2=>array('name'=>'Indeksi kakkonen'),
+#	3=>array('name'=>'Indeksi kolmonen')
+#);
  
 
-for ($i = 1; $i < 500; $i++)
-	$data['data'][$i] = array(1 => 'porsche',2 => 123456.67,3 => "2011-05-13T14:22:46.12+02:00", 'res' => 123, 'mean' => 123, 'sd' => 123);
+#for ($i = 1; $i < 500; $i++)
+#	$data['data'][$i] = array(1 => 'porsche',2 => 123456.67,3 => "2011-05-13T14:22:46.12+02:00", 'res' => 123, 'mean' => 123, 'sd' => 123);
 
 
 
-	$ret = json_decode(do_post_request('http://cl1.opasnet.org/opasnet_base_2/index.php', http_build_query(array('json' => json_encode($header)))));
-	if ($ret && ! isset($ret->error) && ! empty($ret->key)) {
-		echo "Token: " . $ret->key;
-		echo "<br/><br/>POSTing data<br/>";
-		$data['key'] = $ret->key;
-		$ret = do_post_request('http://cl1.opasnet.org/opasnet_base_2/index.php', http_build_query(array('json' => json_encode($data))));
-	}
+#	$ret = json_decode(do_post_request('http://cl1.opasnet.org/opasnet_base_2/index.php', http_build_query(array('json' => json_encode($header)))));
+#	if ($ret && ! isset($ret->error) && ! empty($ret->key)) {
+#		echo "Token: " . $ret->key;
+#		echo "<br/><br/>POSTing data<br/>";
+#		$data['key'] = $ret->key;
+#		$ret = do_post_request('http://cl1.opasnet.org/opasnet_base_2/index.php', http_build_query(array('json' => json_encode($data))));
+#	}
 
 
 
