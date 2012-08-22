977a762d3393e012d13bf184fd5c480b3d438c55
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-08-22 11:25:36 +0000

    Bug splat.
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@108 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/CheckMarginals.r
+++ trunk/R/CheckMarginals.r
@@ -1,7 +1,8 @@
-# CheckMarginals ############# Assumes that all depended upon variables are in memory, as should be the case.
+# CheckMarginals #############
 ##################
 # Returns an ovariable with a marginal devised from the data and upstream variable marginals. 
-# Marginal values for data should be stored into the database somehow
+# Marginal values for data could be stored into the database.
+# Assumes that all depended upon variables are in memory, as should be the case.
 
 CheckMarginals <- function(variable, deps = list(), priormarg = TRUE, indent = 0, verbose = TRUE, ...) { # deps necessary for ops functionality
 	if (verbose) cat(rep("-", indent), "Checking", variable@name, "marginals", "...")
@@ -19,13 +20,17 @@ CheckMarginals <- function(variable, deps = list(), priormarg = TRUE, indent = 0
 	}
 	if (length(deps) > 0) {
 		for (i in deps) {
-			varmar <- unique(c(varmar, colnames(i@output)[i@marginal]))
-			novarmar <- unique(c(novarmar, colnames(i@output)[!i@marginal]))
+			if (class(get(i)) == "ovariable") {
+				varmar <- unique(c(varmar, colnames(i@output)[i@marginal]))
+				novarmar <- unique(c(novarmar, colnames(i@output)[!i@marginal]))
+			}
 		}
 	} else {
 		for (i in as.character(variable@dependencies$Name)){
-			varmar <- unique(c(varmar, colnames(get(i)@output)[get(i)@marginal]))
-			novarmar <- unique(c(novarmar, colnames(get(i)@output)[!get(i)@marginal]))
+			if (class(get(i)) == "ovariable") {
+				varmar <- unique(c(varmar, colnames(get(i)@output)[get(i)@marginal]))
+				novarmar <- unique(c(novarmar, colnames(get(i)@output)[!get(i)@marginal]))
+			}
 		}
 	}
 	varmar <- varmar[!varmar %in% novarmar]
