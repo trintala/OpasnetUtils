f8414dd07dd435c8e961b520d6353ead16326f07
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-11-21 21:33:54 +0000

    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@216 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/Tidy.r
+++ R/Tidy.r
@@ -1,6 +1,9 @@
-# TIDY ########### tidy: a function that cleans the tables from Opasnet Base
-# data is a table from op_baseGetData function
-tidy <- function (data, objname = "", idvar = "obs", direction = "wide") {
+# TIDY ########### tidy: a function that cleans the tables from Opasnet Base. Intended to be used with table2base-uploaded
+# tables, since they have the required Obs/Row - column.
+# inputs:
+#   data - is a table from the base
+#   widecol - can be a vector
+tidy <- function (data, objname = "", idvar = "obs", direction = "wide", widecol = NULL) {
 	data$Result <- ifelse(is.na(data$Result.Text), data$Result, as.character(data$Result.Text))
 	#data <- data[
 	#	ifelse("Observation" %in% colnames(data), 
@@ -14,19 +17,25 @@ tidy <- function (data, objname = "", idvar = "obs", direction = "wide") {
 		data[, "obs"] <- data[, "obs.1"]
 		data <- data[, colnames(data) != "obs.1"]
 	}
+	if("Obs" %in% colnames(data)) { # this line
+		data[, "obs"] <- data[, "Obs"]
+		data <- data[, colnames(data) != "Obs"]
+	}
 	if("Row" %in% colnames(data)) { # If user has given Row, it is used instead of automatic obs.
 		data <- data[, colnames(data) != "obs"]
 		colnames(data)[colnames(data) == "Row"] <- "obs"
 	}
 	#if (objname != "") objname <- paste(objname, "", sep = "")
 	if (direction == "wide") { 
-		if("Observation" %in% colnames(data)) {
-			widecol <- "Observation"
-		} else if("Parameter" %in% colnames(data)) {
-			widecol <- "Parameter"
-		} else if("Havainto" %in% colnames(data)) {
-			widecol <- "Havainto"
-		} else widecol <- NA
+		if(is.null(widecol)) {
+			if("Observation" %in% colnames(data)) {
+				widecol <- "Observation"
+			} else if("Parameter" %in% colnames(data)) {
+				widecol <- "Parameter"
+			} else if("Havainto" %in% colnames(data)) {
+				widecol <- "Havainto"
+			} else widecol <- NA
+		}
 		if(!is.na(widecol)) {
 			cols <- levels(data$Observation)
 			data <- reshape(data, idvar = idvar, timevar = widecol, v.names = "Result", direction = "wide")
