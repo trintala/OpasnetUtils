dd3211339a038c46e45e8f44d66a65e86a206f0e
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-11-24 21:19:55 +0000

    Noniin, ongelma siis oli Result.text puuttuminen base2 outputissa. Muutin defaultit sopivaksi base2 datalle, mut jätin myös base1 yhteensopivuutta varten base1 parametrin.
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@224 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/Tidy.r
+++ trunk/R/Tidy.r
@@ -11,27 +11,25 @@
 #     system to store the original row number. This can be used as such in reshape to bring out the original table.
 #   direction - reshape paramater that tells that we are 'widening' the dataframe
 #   widecol - the column which contains 'new' columns
-tidy.old <- function (data, objname = "", idvar = "obs", direction = "wide", widecol = NULL) {
-	data$Result <- ifelse(is.na(data$Result.Text), data$Result, as.character(data$Result.Text))
-	#data <- data[
-	#	ifelse("Observation" %in% colnames(data), 
-	#		data$Observation != "Description",
-	#		TRUE
-	#	), 
-	#	!colnames(data) %in% c("id", "Result.Text")
-	#]
-	data <- data[, !colnames(data) %in% c("id", "Result.Text")]
-	if("obs.1" %in% colnames(data)) { # this line is temporarily needed until the obs.1 bug is fixed.
-		data[, "obs"] <- data[, "obs.1"]
+#	base1 - indicates whether data is split in result and result.text like in the old database
+####################################################################################################################
+tidy <- function (data, objname = "", idvar = "Obs", direction = "wide", widecol = NULL, base1 = FALSE) {
+	if(base1){
+		data$Result <- ifelse(is.na(data$Result.Text), data$Result, as.character(data$Result.Text))
+		data <- data[, !colnames(data) %in% c("id", "Result.Text")]
+	}
+	# The following lines are there for base1 legacy functionality
+	if("obs.1" %in% colnames(data)) {
+		data[, "Obs"] <- data[, "obs.1"]
 		data <- data[, colnames(data) != "obs.1"]
-	} else if("Obs" %in% colnames(data)) {
-		data[, "obs"] <- data[, "Obs"]
+	} else if("obs" %in% colnames(data)) {
+		data[, "Obs"] <- data[, "obs"]
 		data <- data[, colnames(data) != "Obs"]
 	} else if("Row" %in% colnames(data)) { # If user has given Row, it is used instead of automatic obs.
-		data <- data[, colnames(data) != "obs"]
-		colnames(data)[colnames(data) == "Row"] <- "obs"
+		data[, "Obs"] <- data [,"Row"]
+		data <- data[, colnames(data) != "Row"]
 	}
-	#if (objname != "") objname <- paste(objname, "", sep = "")
+	
 	if (direction == "wide") { 
 		if(is.null(widecol)) {
 			if("Observation" %in% colnames(data)) {
@@ -45,60 +43,7 @@ tidy.old <- function (data, objname = "", idvar = "obs", direction = "wide", wid
 		if(!is.na(widecol)) {
 			cols <- levels(data$Observation)
 			data <- reshape(data, idvar = idvar, timevar = widecol, v.names = "Result", direction = "wide")
-			data <- data[colnames(data) != "obs"]
-			colnames(data) <- gsub("^Result.", objname, colnames(data))
-			for (i in paste(objname, cols, sep = "")) {
-				a <- suppressWarnings(as.numeric(as.character(data[, i])))
-				if (sum(is.na(a)) == 0) data[, i] <- a else data[, i] <- factor(data[, i])
-			}
-			colnames(data)[grepl(paste("^", objname, "result", sep = ""), colnames(data))] <- paste(objname, "Result", sep = "")
-			colnames(data)[grepl(paste("^", objname, "Amount", sep = ""), colnames(data))] <- paste(objname, "Result", sep = "")
-			return(data)
-		}
-	}
-	data <- data[,colnames(data) != "obs"]
-	colnames(data)[colnames(data)=="Result"] <- paste(objname, "Result", sep = "")
-	return(data)
-}
-
-
-
-
-tidy <- function (data, objname = "", idvar = "Obs", direction = "wide", widecol = NULL) {
-	#data$Result <- ifelse(is.na(data$Result.Text), data$Result, as.character(data$Result.Text))
-	#data <- data[
-	#	ifelse("Observation" %in% colnames(data), 
-	#		data$Observation != "Description",
-	#		TRUE
-	#	), 
-	#	!colnames(data) %in% c("id", "Result.Text")
-	#]
-	#data <- data[, !colnames(data) %in% c("id", "Result.Text")]
-	#if("obs.1" %in% colnames(data)) { # this line is temporarily needed until the obs.1 bug is fixed.
-	#	data[, "obs"] <- data[, "obs.1"]
-	#	data <- data[, colnames(data) != "obs.1"]
-	#} else if("Obs" %in% colnames(data)) { # this line
-	#	data[, "obs"] <- data[, "Obs"]
-	#	data <- data[, colnames(data) != "Obs"]
-	#} else if("Row" %in% colnames(data)) { # If user has given Row, it is used instead of automatic obs.
-	#	data <- data[, colnames(data) != "obs"]
-	#	colnames(data)[colnames(data) == "Row"] <- "obs"
-	#}
-	#if (objname != "") objname <- paste(objname, "", sep = "")
-	if (direction == "wide") { 
-		if(is.null(widecol)) {
-			if("Observation" %in% colnames(data)) {
-				widecol <- "Observation"
-			} else if("Parameter" %in% colnames(data)) {
-				widecol <- "Parameter"
-			} else if("Havainto" %in% colnames(data)) {
-				widecol <- "Havainto"
-			} else widecol <- NA
-		}
-		if(!is.na(widecol)) {
-			cols <- levels(data$Observation)
-			data <- reshape(data, idvar = idvar, timevar = widecol, v.names = "Result", direction = "wide")
-			data <- data[colnames(data) != "Obs"]
+			#data <- data[colnames(data) != idvar]
 			colnames(data) <- gsub("^Result.", objname, colnames(data))
 			for (i in paste(objname, cols, sep = "")) {
 				a <- suppressWarnings(as.numeric(as.character(data[, i])))
