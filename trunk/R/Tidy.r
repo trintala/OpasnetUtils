d31d1767927af12eff7b3261e23e6970a1b0be13
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2012-11-24 20:47:42 +0000

    Korjasin jo sen ongelman, ja tää Einarin fiksi kusee pari aikasempaa koodia (esim. vesiopas).
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@222 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/Tidy.r
+++ trunk/R/Tidy.r
@@ -11,7 +11,7 @@
 #     system to store the original row number. This can be used as such in reshape to bring out the original table.
 #   direction - reshape paramater that tells that we are 'widening' the dataframe
 #   widecol - the column which contains 'new' columns
-tidy.old <- function (data, objname = "", idvar = "obs", direction = "wide", widecol = NULL) {
+tidy <- function (data, objname = "", idvar = "obs", direction = "wide", widecol = NULL) {
 	data$Result <- ifelse(is.na(data$Result.Text), data$Result, as.character(data$Result.Text))
 	#data <- data[
 	#	ifelse("Observation" %in% colnames(data), 
@@ -24,7 +24,7 @@ tidy.old <- function (data, objname = "", idvar = "obs", direction = "wide", wid
 	if("obs.1" %in% colnames(data)) { # this line is temporarily needed until the obs.1 bug is fixed.
 		data[, "obs"] <- data[, "obs.1"]
 		data <- data[, colnames(data) != "obs.1"]
-	} else if("Obs" %in% colnames(data)) { # this line
+	} else if("Obs" %in% colnames(data)) {
 		data[, "obs"] <- data[, "Obs"]
 		data <- data[, colnames(data) != "Obs"]
 	} else if("Row" %in% colnames(data)) { # If user has given Row, it is used instead of automatic obs.
@@ -64,7 +64,7 @@ tidy.old <- function (data, objname = "", idvar = "obs", direction = "wide", wid
 
 
 
-tidy <- function (data, objname = "", idvar = "Obs", direction = "wide", widecol = NULL) {
+#tidy <- function (data, objname = "", idvar = "Obs", direction = "wide", widecol = NULL) {
 	#data$Result <- ifelse(is.na(data$Result.Text), data$Result, as.character(data$Result.Text))
 	#data <- data[
 	#	ifelse("Observation" %in% colnames(data), 
@@ -85,31 +85,34 @@ tidy <- function (data, objname = "", idvar = "Obs", direction = "wide", widecol
 	#	colnames(data)[colnames(data) == "Row"] <- "obs"
 	#}
 	#if (objname != "") objname <- paste(objname, "", sep = "")
-	if (direction == "wide") { 
-		if(is.null(widecol)) {
-			if("Observation" %in% colnames(data)) {
-				widecol <- "Observation"
-			} else if("Parameter" %in% colnames(data)) {
-				widecol <- "Parameter"
-			} else if("Havainto" %in% colnames(data)) {
-				widecol <- "Havainto"
-			} else widecol <- NA
-		}
-		if(!is.na(widecol)) {
-			cols <- levels(data$Observation)
-			data <- reshape(data, idvar = idvar, timevar = widecol, v.names = "Result", direction = "wide")
-			data <- data[colnames(data) != "Obs"]
-			colnames(data) <- gsub("^Result.", objname, colnames(data))
-			for (i in paste(objname, cols, sep = "")) {
-				a <- suppressWarnings(as.numeric(as.character(data[, i])))
-				if (sum(is.na(a)) == 0) data[, i] <- a else data[, i] <- factor(data[, i])
-			}
-			colnames(data)[grepl(paste("^", objname, "result", sep = ""), colnames(data))] <- paste(objname, "Result", sep = "")
-			colnames(data)[grepl(paste("^", objname, "Amount", sep = ""), colnames(data))] <- paste(objname, "Result", sep = "")
-			return(data)
-		}
-	}
-	data <- data[,colnames(data) != "Obs"]
-	colnames(data)[colnames(data)=="Result"] <- paste(objname, "Result", sep = "")
-	return(data)
-}
+	#########################################
+	##### New ###############################
+	#########################################
+	#if (direction == "wide") { 
+	#	if(is.null(widecol)) {
+	#		if("Observation" %in% colnames(data)) {
+	#			widecol <- "Observation"
+	#		} else if("Parameter" %in% colnames(data)) {
+	#			widecol <- "Parameter"
+	#		} else if("Havainto" %in% colnames(data)) {
+	#			widecol <- "Havainto"
+	#		} else widecol <- NA
+	#	}
+	#	if(!is.na(widecol)) {
+	#		cols <- levels(data$Observation)
+	#		data <- reshape(data, idvar = idvar, timevar = widecol, v.names = "Result", direction = "wide")
+	#		data <- data[colnames(data) != "Obs"]
+	#		colnames(data) <- gsub("^Result.", objname, colnames(data))
+	#		for (i in paste(objname, cols, sep = "")) {
+	#			a <- suppressWarnings(as.numeric(as.character(data[, i])))
+	#			if (sum(is.na(a)) == 0) data[, i] <- a else data[, i] <- factor(data[, i])
+	#		}
+	#		colnames(data)[grepl(paste("^", objname, "result", sep = ""), colnames(data))] <- paste(objname, "Result", sep = "")
+	#		colnames(data)[grepl(paste("^", objname, "Amount", sep = ""), colnames(data))] <- paste(objname, "Result", sep = "")
+	#		return(data)
+	#	}
+	#}
+	#data <- data[,colnames(data) != "Obs"]
+	#colnames(data)[colnames(data)=="Result"] <- paste(objname, "Result", sep = "")
+	#return(data)
+#}
