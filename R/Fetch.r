93c96a695e608ee0e8ae0eea86825f84a4913b31
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-01-13 19:15:38 +0000

    New fetching feature: dependencies table 'Ident' - column allows Fetch2 to download the most recent public object on the relevant parent site.
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@247 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/Fetch.r
+++ R/Fetch.r
@@ -17,18 +17,25 @@ Fetch2 <- function(dependencies, evaluate = FALSE, indent = 0, verbose = TRUE, .
 	if (nrow(dependencies) > 0) {
 		for (i in 1:nrow(dependencies)) {
 			if(!exists(as.character(dependencies$Name[i]))) {
-				if (if (is.null(dependencies$Key[i])) TRUE else is.na(dependencies$Key[i]) | dependencies$Key[i] == "") {
-					stop(paste("No key given for dependent variable", dependencies$Name))
+				testkey <- if (is.null(dependencies$Key[i])) TRUE else is.na(dependencies$Key[i]) | dependencies$Key[i] == "")
+				testid <- if (is.null(dependencies$Ident[i])) TRUE else is.na(dependencies$Ident[i]) | dependencies$Ident[i] == "")
+				if (testkey & testid) {
+					stop(paste("No key nor ident given for dependent variable: ", dependencies$Name, "!", sep = ""))
 				}
-				objects.get(dependencies$Key[i]) # Key is the R-tools session identifier (shown at the end of the url)
+				if (testkey) {
+					objects.get(dependencies$Key[i]) # Key is the R-tools session identifier (shown at the end of the url)
+				}
+				if (!testkey & testid) {
+					ident <- strsplit(dependencies$Ident[i], "/")[[1]] # Ident should be in format <page_id>/<code_name>
+					objects.get_latest(ident[1], ident[2])
+				}
 				if (evaluate) assign(
-					as.character(dependencies$Name[i]), 
-					EvalOutput(get(as.character(dependencies$Name[i])), ...), 
-					envir = .GlobalEnv
-				) 
-				# Eval not necessarily needed at this point
+						as.character(dependencies$Name[i]), 
+						EvalOutput(get(as.character(dependencies$Name[i])), ...), 
+						envir = .GlobalEnv
+				)
 				if (verbose) cat("\n", rep("-", indent), as.character(dependencies$Name[i]), "fetched successfully!\n")
 			}
 		}
 	}
-} # no need to return anything since variables are already written in global memory
+} # no need to return anything since variables are written in global memory by objects.get
