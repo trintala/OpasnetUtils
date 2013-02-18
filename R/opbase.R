ffc9ea92a3923e704ca893fcfd6a1c93b60a96c8
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-02-18 14:04:20 +0000

    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@282 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/opbase.R
+++ R/opbase.R
@@ -79,16 +79,16 @@ opbase.data <- function(ident, series_id = NULL, verbose = FALSE, username = NUL
 				tmp <- list()
 				for (rivi in temp) {
 					for (sarake in names(rivi)) {
-						if (verbose)cat(sarake, ",")
-						if (verbose)cat(rivi[[sarake]], "\n")
-						temp[[sarake]][length(tmp[[sarake]]) + 1] <- rivi[[sarake]]
+						#if (verbose)cat(sarake, ",")
+						#if (verbose)cat(rivi[[sarake]], "\n")
+						tmp[[sarake]][length(tmp[[sarake]]) + 1] <- rivi[[sarake]]
 					}
 				}
 				out <- rbind(out, data.frame(tmp))
 				#if (verbose) cat(names(tmp), ",")
 				#if (verbose) cat(length(tmp[[1]]), ",")
-				stop("Backstop")
-				if (verbose) cat(nrow(out), "\n")
+				#stop("Backstop")
+				#if (verbose) cat(nrow(out), "\n")
 			} else {
 				if (verbose) print(paste('JSON parsed',format(Sys.time(), "%H:%M:%OS3"),sep=''))
 				temp <- lapply(temp, data.frame)#list.to.data.frame)	# THIS FUNCTION IS RELATIVELY SLOW!!!! Could this be done in any other way?
