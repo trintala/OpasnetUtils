663164d345c3ee8ed3d0abb771c784c54197e593
ahappone <ahappone@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-02-26 13:18:46 +0000

    missing file added
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@285 3c636ae5-be99-4508-8416-6ebf76334c49

---
+++ trunk/R/OPrint.r
@@ -0,0 +1,24 @@
+oprint <- function(x, show_all = FALSE, sortable = TRUE) {
+	if (nrow(x) > 1000 && !show_all)
+	{
+		print(paste('Showing first 1000 rows out of ',nrow(x),'. Set show_all=TRUE to show all rows.',sep=''))
+		x = x[1:1000,]
+	}
+	
+	if (sortable){
+		print(xtable(x), type = 'html', html.table.attributes="class='wikitable sortable'")
+	} else {
+		print(xtable(x), type = 'html', html.table.attributes="class='wikitable'")
+	}
+}
+
+setGeneric("oprint")
+
+setMethod(
+		f = "oprint",
+		signature = signature(x = "ovariable"),
+		definition = function(x, show_all = FALSE, sortable = TRUE) {
+			x <- EvalOutput(x)
+			callGeneric(x@output, show_all = show_all, sortable = sortable)
+		}
+)
