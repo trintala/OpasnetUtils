169d2fc81b6f2d10e7f9e37c297f1aa9c12b6f4e
ahappone <ahappone@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-03-12 14:57:08 +0000

    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@296 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/ComputeDependencies.r
+++ trunk/R/ComputeDependencies.r
@@ -6,14 +6,16 @@ ComputeDependencies <- function(dependencies, forceEval = FALSE, indent = 0, ...
 	cat("\n")
 	Fetch2(dependencies, indent = indent, ...)
 	for (i in as.character(dependencies$Name)) {
+		# First check if dependency exists at all
+		ret <- tryCatch(get(i), error = function(e) return(NULL))
+		if (is.null(ret)) stop(paste("Ovariable depends on missing variable named '",i,"'",sep='' ))
+		# If dependency is ovariable?
 		if (class(get(i)) == "ovariable") {
 			if (nrow(get(i)@output) == 0 | forceEval) assign(i, EvalOutput(get(i), indent = indent, ...), envir = .GlobalEnv)
 			assign(i, CheckMarginals(get(i), indent = indent, ...), envir = .GlobalEnv)
 			assign(i, CheckInput(get(i), indent = indent, ...), envir = .GlobalEnv)
 			assign(i, CheckDecisions(get(i), indent = indent, ...), envir = .GlobalEnv)
 			assign(i, CheckCollapse(get(i), indent = indent, ...), envir = .GlobalEnv)
-		} else {
-			stop(paste("Ovariable '", i, "' not found!", sep=''))
 		}
 	}
 	cat("\n")
