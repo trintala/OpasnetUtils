37ade9880afdce93b1e12dedaded75eb0a374ba7
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-03-24 13:32:28 +0000

    Call structure changed so that ComputeDependencies is no longer needed at the formula level.
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@303 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/ComputeDependencies.r
+++ trunk/R/ComputeDependencies.r
@@ -1,22 +1,27 @@
- # ComputeDependencies ############ uses Fetch2, EvalOutput, CheckMarginals and CheckInput to load and pre-process
-# upstream variables. Typically seen on the first line of ovariable formula code. 
+# ComputeDependencies ############ uses Fetch2, EvalOutput, CheckMarginals and CheckInput to load and pre-process
+# upstream variables. Is used automatically by EvalOutput, but can be seen on the first lines of old ovariable formula code, 
+# to avoid applying decisions, inputs and optimizations twice in old code the function does nothing by default. This is no 
+# problem since users should not be calling this function at all.
 # '...' can be used for input substitution, na.rm, number of iterations (N) and others
+# new_code is a parameter for old codes where ComputeDependencies was not automated
 
-ComputeDependencies <- function(dependencies, forceEval = FALSE, indent = 0, ...) { 
-	cat("\n")
-	Fetch2(dependencies, indent = indent, ...)
-	for (i in as.character(dependencies$Name)) {
-		# First check if dependency exists at all
-		ret <- tryCatch(get(i), error = function(e) return(NULL))
-		if (is.null(ret)) stop(paste("Ovariable depends on missing variable named '",i,"'",sep='' ))
-		# If dependency is ovariable?
-		if (class(get(i)) == "ovariable") {
-			if (nrow(get(i)@output) == 0 | forceEval) assign(i, EvalOutput(get(i), indent = indent, ...), envir = .GlobalEnv)
-			assign(i, CheckMarginals(get(i), indent = indent, ...), envir = .GlobalEnv)
-			assign(i, CheckInput(get(i), indent = indent, ...), envir = .GlobalEnv)
-			assign(i, CheckDecisions(get(i), indent = indent, ...), envir = .GlobalEnv)
-			assign(i, CheckCollapse(get(i), indent = indent, ...), envir = .GlobalEnv)
+ComputeDependencies <- function(dependencies, forceEval = FALSE, indent = 0, new_code = FALSE, ...) { 
+	if (new_code) {
+		cat("\n")
+		Fetch2(dependencies, indent = indent, ...)
+		for (i in as.character(dependencies$Name)) {
+			# First check if dependency exists at all
+			ret <- tryCatch(get(i), error = function(e) return(NULL))
+			if (is.null(ret)) stop(paste("Ovariable depends on missing variable named '",i,"'",sep='' ))
+			# If dependency is ovariable
+			if (class(get(i)) == "ovariable") {
+				if (nrow(get(i)@output) == 0 | forceEval) assign(i, EvalOutput(get(i), indent = indent, ...), envir = .GlobalEnv)
+				#assign(i, CheckMarginals(get(i), indent = indent, ...), envir = .GlobalEnv) # moved to EvalOutput
+				assign(i, CheckInput(get(i), indent = indent, ...), envir = .GlobalEnv)
+				assign(i, CheckDecisions(get(i), indent = indent, ...), envir = .GlobalEnv)
+				assign(i, CheckCollapse(get(i), indent = indent, ...), envir = .GlobalEnv)
+			}
 		}
+		cat("\n")
 	}
-	cat("\n")
 }
