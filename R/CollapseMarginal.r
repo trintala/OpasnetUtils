310412524c2608949cfe8df246cf46a07c6ac204
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-04-04 20:03:08 +0000

    bugfix
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@324 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/CollapseMarginal.r
+++ R/CollapseMarginal.r
@@ -37,7 +37,7 @@ CollapseMarginal <- function(variable, cols, probs = NULL, ...) { # cols is a ch
 		for (i in cols) {
 			a <- a + 1
 			b <- probs[[a]]
-			if(is.null(b)) b <- rep(1, N)
+			if(is.null(b)) b <- rep(1, length(unique(as.character(out[[i]]))))
 			#selection <- tapply( # For each iteration, sample from available locations. All locations should exist for
 			#	out[[i]], # all iterations, but tapply is the most efficient way of doing the sampling and it adds
 			#	out[["Iter"]], # extra robustness. Not going to work with given probabilities, since the input would 
