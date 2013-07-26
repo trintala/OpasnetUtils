ed3f8257c50517c987b9d79e5f7d624a3c09b46c
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-07-26 11:58:30 +0000

    opasnet.R is potentially dangerous
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils/trunk@370 3c636ae5-be99-4508-8416-6ebf76334c49

--- R/ORBind.r
+++ R/ORBind.r
@@ -4,25 +4,36 @@ orbind <- function(x, y) {
 	if(class(x) == "ovariable") x <- x@output
 	if(class(y) == "ovariable") y <- y@output
 	
-	addmissingcol <- function(e1, e2) { #Adds all missing columns. Merges Iter if that is missing.
-		cols <- setdiff(colnames(e2), colnames(e1)) # Take all columns that do not exist in e1.
-		
-		if("Iter" %in% cols) {
-			e1 <- merge(unique(e2["Iter"]), e1) # Add Iter with all locations existing in e2.
-			cols <- cols[cols != "Iter"] # Remove Iter from the list of columns to add.
-		}
-		col <- as.data.frame(array(NA, dim = c(1, length(cols))))
-		colnames(col) <- cols
-		if("Unit" %in% cols) {col[, "Unit"] <- "?"}
-		
-		return(cbind(e1, col))
-	}
-	
-	temp1 <- addmissingcol(x, y)
+	temp1 <- addmissingcol(x, y) # See below
 	temp2 <- addmissingcol(y, x)
 	
 	return(rbind(temp1, temp2))
 	
 #Should this be made S4 function for ovariables? Then it could be named simply rbind.
+	
+}
+
+addmissingcol <- function(e1, e2) { #Adds all missing columns. Merges Iter if that is missing.
+	cols <- setdiff(colnames(e2), colnames(e1)) # Take all columns that do not exist in e1.
+	
+	if("Iter" %in% cols) {
+		e1 <- merge(unique(e2["Iter"]), e1) # Add Iter with all locations existing in e2.
+		cols <- cols[cols != "Iter"] # Remove Iter from the list of columns to add.
+	}
+	col <- as.data.frame(array(NA, dim = c(1, length(cols))))
+	colnames(col) <- cols
+	if("Unit" %in% cols) {col[, "Unit"] <- "?"}
+	
+	return(cbind(e1, col))
+}
+
+# Combine
+
+combine <- function(...) {
+	variable_list <- list(...)
+	marginals <- character()
+	for (i in variable_list) {
+		marginals <- c(marginals, colnames(i@output)[i@marginal])
+	}
 	
 }
