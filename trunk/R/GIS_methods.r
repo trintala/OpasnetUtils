00c42b04b79f723f1d37a93e57b1b2d8026909df
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-08-27 15:22:58 +0000

    Coordinate conversion functions for the finnish population data added
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@378 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/GIS_methods.r
+++ trunk/R/GIS_methods.r
@@ -337,3 +337,122 @@ GIS.Concentration.matrix <- function(
 	out <- PILTTI.matrix * Emission
 	return(out)
 }
+
+
+
+deg2rad <- function(x) {x*2*pi/360}
+rad2deg <- function(x) {x*360/(2*pi)}
+
+# Muunnosfunktiot koordinaattiprojektioille
+# ETRS89-TM35FIN, geodeettisista tasokoordinaateiksi ja takaisin
+# Lähde: JHS 154, 6.6.2008
+# http://www.jhs-suositukset.fi/web/guest/jhs/recommendations/154
+# 2013-04-29/JeH, loukko (at) loukko (dot) net
+# http://www.loukko.net/koord_proj/
+# Vapaasti käytettävissä ilman toimintatakuuta.
+
+# Muuntaa desimaalimuotoiset leveys- ja pituusasteet ETRS-TM35FIN -muotoisiksi tasokoordinaateiksi
+# Esim.
+# koordGT(60.385106872, 19.848136769) --> array(2) { ["N"]=> float(106256.3597039) ["E"]=> float(6715706.37704) }
+
+koordGT <- function(lev_aste, pit_aste) {
+	
+	# Vakiot
+	f = 1 / 298.257222101		# Ellipsoidin litistyssuhde
+	a = 6378137					# Isoakselin puolikas
+	lambda_nolla = 0.471238898	# Keskimeridiaani (rad), 27 astetta
+	k_nolla = 0.9996			# Mittakaavakerroin
+	E_nolla = 500000			# Itäkoordinaatti
+	
+	# Kaavat
+	
+	# Muunnetaan astemuotoisesta radiaaneiksi
+	fii = deg2rad(lev_aste)
+	lambda = deg2rad(pit_aste)
+	
+	n = f / (2-f)
+	A1 = (a/(1+n)) * (1 + (n^2/4) + (n^4/64))
+	e_toiseen = (2 * f) - f^2
+	e_pilkku_toiseen = e_toiseen / (1 - e_toiseen)
+	h1_pilkku = (1/2)*n - (2/3)*n^2 + (5/16)*n^3 + (41/180)*n^4
+	h2_pilkku = (13/48)*n^2 - (3/5)*n^3 + (557/1440)*n^4
+	h3_pilkku =(61/240)*n^3 - (103/140)*n^4
+	h4_pilkku = (49561/161280)*n^4
+	Q_pilkku = asinh(tan(fii))
+	Q_2pilkku = atanh(sqrt(e_toiseen) * sin(fii))
+	Q = Q_pilkku - sqrt(e_toiseen) * Q_2pilkku
+	l = lambda - lambda_nolla
+	beeta = atan(sinh(Q))
+	eeta_pilkku = atanh(cos(beeta) * sin(l))
+	zeeta_pilkku = asin(sin(beeta)/(1/cosh(eeta_pilkku)))
+	zeeta1 = h1_pilkku * sin( 2 * zeeta_pilkku) * cosh( 2 * eeta_pilkku)
+	zeeta2 = h2_pilkku * sin( 4 * zeeta_pilkku) * cosh( 4 * eeta_pilkku)
+	zeeta3 = h3_pilkku * sin( 6 * zeeta_pilkku) * cosh( 6 * eeta_pilkku)
+	zeeta4 = h4_pilkku * sin( 8 * zeeta_pilkku) * cosh( 8 * eeta_pilkku)
+	eeta1 = h1_pilkku * cos( 2 * zeeta_pilkku) * sinh( 2 * eeta_pilkku)
+	eeta2 = h2_pilkku * cos( 4 * zeeta_pilkku) * sinh( 4 * eeta_pilkku)
+	eeta3 = h3_pilkku * cos( 6 * zeeta_pilkku) * sinh( 6 * eeta_pilkku)
+	eeta4 = h4_pilkku * cos( 8 * zeeta_pilkku) * sinh( 8 * eeta_pilkku)
+	zeeta = zeeta_pilkku + zeeta1 + zeeta2 + zeeta3 + zeeta4
+	eeta = eeta_pilkku + eeta1 + eeta2 + eeta3 + eeta4
+	
+	# Tulos tasokoordinaatteina
+	N = A1 * zeeta * k_nolla
+	E = A1 * eeta * k_nolla + E_nolla
+	
+	return(data.frame(N, E))
+}
+
+
+# koordTG 
+# Muuntaa ETRS-TM35FIN -muotoiset tasokoordinaatit desimaalimuotoisiksi leveys- ja pituusasteiksi
+# koordTG(106256.35958, 6715706.37705) --> array(2) { ["lev"]=> float(60.38510687197) ["pit"]=> float(19.848136766751) }
+
+koordTG<- function(N, E) {
+	
+	# Vakiot	
+	f = 1 / 298.257222101				# Ellipsoidin litistyssuhde
+	a = 6378137									# Isoakselin puolikas
+	lambda_nolla = 0.471238898	# Keskimeridiaani (rad), 27 astetta
+	k_nolla = 0.9996						# Mittakaavakerroin
+	E_nolla = 500000						# Itäkoordinaatti
+	
+	# Kaavat
+	n = f / (2-f)
+	A1 = (a/(1+n)) * (1 + (n^2/4) + (n^4/64))
+	e_toiseen = (2 * f) - f^2
+	h1 = (1/2)*n - (2/3)*n^2 + (37/96)*n^3 - (1/360)*n^4
+	h2 = (1/48)*n^2 + (1/15)*n^3 - (437/1440)*n^4
+	h3 =(17/480)*n^3 - (37/840)*n^4
+	h4 = (4397/161280)*n^4
+	zeeta = N / (A1 * k_nolla)
+	eeta = (E - E_nolla) / (A1 * k_nolla)
+	zeeta1_pilkku = h1 * sin( 2 * zeeta) * cosh( 2 * eeta)
+	zeeta2_pilkku = h2 * sin( 4 * zeeta) * cosh( 4 * eeta)
+	zeeta3_pilkku = h3 * sin( 6 * zeeta) * cosh( 6 * eeta)
+	zeeta4_pilkku = h4 * sin( 8 * zeeta) * cosh( 8 * eeta)
+	eeta1_pilkku = h1 * cos( 2 * zeeta) * sinh( 2 * eeta)
+	eeta2_pilkku = h2 * cos( 4 * zeeta) * sinh( 4 * eeta)
+	eeta3_pilkku = h3 * cos( 6 * zeeta) * sinh( 6 * eeta)
+	eeta4_pilkku = h4 * cos( 8 * zeeta) * sinh( 8 * eeta)
+	zeeta_pilkku = zeeta - (zeeta1_pilkku + zeeta2_pilkku + zeeta3_pilkku + zeeta4_pilkku)
+	eeta_pilkku = eeta - (eeta1_pilkku + eeta2_pilkku + eeta3_pilkku + eeta4_pilkku)
+	beeta = asin((1/cosh(eeta_pilkku)*sin(zeeta_pilkku)))
+	l = asin(tanh(eeta_pilkku)/(cos(beeta)))
+	Q = asinh(tan(beeta))
+	Q_pilkku = Q + sqrt(e_toiseen) * atanh(sqrt(e_toiseen) * tanh(Q))
+	
+	#for (kierros in 1:5) {
+	#	Q_pilkku = Q + sqrt(e_toiseen) * atanh(sqrt(e_toiseen) * tanh(Q_pilkku))
+	#}
+	
+	# Tulos radiaaneina
+	fii = atan(sinh(Q_pilkku))
+	lambda = lambda_nolla + l
+	
+	# Tulos asteina
+	fii = rad2deg(fii)
+	lambda = rad2deg(lambda)
+	
+	return(data.frame(LA = fii, LO = lambda))
+}
