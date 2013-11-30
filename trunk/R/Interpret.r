d5193556211d281845ce0bafe991f4a0286ea7cf
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2013-11-30 12:06:33 +0000

    "NA" and whitespace interpreted as NA
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@394 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/Interpret.r
+++ trunk/R/Interpret.r
@@ -112,6 +112,9 @@ input.interp <- function(res.char, n = 1000, dbug = FALSE) {
 	fromzero <- gregexpr("<", res.char)
 	out <- list()
 	for(i in 1:length(res.char)) {
+		if(res.char[i] %in% c("NA") | nchar(gsub(" ", "", res.char[i])) == 0) {
+			out[[i]] <- rep(NA, n)
+		}
 		val <- suppressWarnings(as.numeric(res.char[i]))
 		if(is.na(val)) {
 			minus.relevant <- unlist(minus)[(cumsum(c(0, minus.length)) + 1)[i]:cumsum(minus.length)[i]]
