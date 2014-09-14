7fc7829a751266463ac908c5c267bb684dec707a
trintala <trintala@3c636ae5-be99-4508-8416-6ebf76334c49>
2014-09-14 11:36:25 +0000

    Merging done via plyr::join and tapplying through closures and plyr::ddply. Should result in better performance.
    git-svn-id: https://www.opasnet.org/svn/opasnet_utils@435 3c636ae5-be99-4508-8416-6ebf76334c49

--- trunk/R/Oapply.r
+++ trunk/R/Oapply.r
@@ -4,42 +4,72 @@
 ### X an ovariable
 ### cols overrides INDEX by choosing INDEX as all marginals NOT given in cols (character vector) argument
 
-oapply = function(X, INDEX = NULL, FUN = NULL, cols = NULL, ..., simplify = TRUE) {
+oapply = function(X, INDEX = NULL, FUN = NULL, cols = NULL, use_plyr = TRUE, ..., simplify = TRUE) {
 	out <- X@output
 	marginals <- colnames(out)[X@marginal]
 	if (is.null(INDEX) & is.null(cols)) stop("No INDEX nor cols defined!\n")
 	if (!is.null(cols)) INDEX <- out[marginals[!marginals %in% cols]]
 	if (length(INDEX) == 0) stop("No marginals!\n")
-	out <- tapply(
-		X = out[[paste(X@name, "Result", sep = "")]], 
-		INDEX = INDEX,
-		FUN = FUN,
-		...,
-		simplify = simplify
-	)
-	if (length(out) == 0) stop("0 length return from tapply!\n")
-	if (is.list(out[1])) { # function returned array
-		rows <- tapply(1:nrow(X@output), INDEX, I)
-		if (length(dim(out[[1]])) == 2) {
-			out <- lapply(out, t)
+	if (use_plyr == TRUE) {
+		if (is.character(INDEX)) vars <- INDEX else vars <- colnames(INDEX)
+		if (is.null(vars)) stop("Unable to determine index name, please use character input.")
+		out <- ddply(
+			out, 
+			vars,
+			oapplyf(FUN),
+			rescol = paste(X@name, "Result", sep = ""),
+			datvars = vars, 
+			.drop = TRUE
+		)
+	} else {
+		# Old implementation
+		out <- tapply(
+			X = out[[paste(X@name, "Result", sep = "")]], 
+			INDEX = INDEX,
+			FUN = FUN,
+			...,
+			simplify = simplify
+		)
+		if (length(out) == 0) stop("0 length return from tapply!\n")
+		if (is.list(out[1])) { # function returned array
+			rows <- tapply(1:nrow(X@output), INDEX, I)
+			if (length(dim(out[[1]])) == 2) {
+				out <- lapply(out, t)
+			}
+			out <- lapply(lapply(out, as.table), as.data.frame)
+			for (i in 1:length(out)) {
+				out[[i]]$Row <- rows[[i]]
+			}
+			out <- do.call(rbind, out)
+			
+			temp <- X@output[!colnames(X@output) %in% paste(X@name, "Result", sep = "")]
+			temp$Row <- 1:nrow(temp)
+			
+			out <- merge(temp, out)
+			out <- out[colnames(out) != "Row"]
 		}
-		out <- lapply(lapply(out, as.table), as.data.frame)
-		for (i in 1:length(out)) {
-			out[[i]]$Row <- rows[[i]]
+		else { # function returned single value
+			out <- as.data.frame(as.table(out))
 		}
-		out <- do.call(rbind, out)
-		
-		temp <- X@output[!colnames(X@output) %in% paste(X@name, "Result", sep = "")]
-		temp$Row <- 1:nrow(temp)
-		
-		out <- merge(temp, out)
-		out <- out[colnames(out) != "Row"]
+		colnames(out)[colnames(out) == "Freq"] <- paste(X@name, "Result", sep = "")
 	}
-	else { # function returned single value
-		out <- as.data.frame(as.table(out))
-	}
-	colnames(out)[colnames(out) == "Freq"] <- paste(X@name, "Result", sep = "")
 	X@output <- out
 	X@marginal <- colnames(out) %in% marginals # Marginals can be easily corrected here disrequiring CheckMarginals
 	return(X)
+}
+
+oapplyf <- function(fun) {
+	out <- function(dat, rescol, datvars) {
+		# Take first entry of each index (since they should contain only one distinct value)
+		out <- data.frame(dat[[datvars[1]]][1])
+		if (length(datvars > 1)) {
+			for (i in 2:length(datvars)) {
+				out[[i]] <- dat[[datvars[i]]][1]
+			}
+		}
+		out <- data.frame(out, fun(dat[[rescol]]))
+		colnames(out) <- c(datvars, rescol)
+		return(out)
+	}
+	return(out)
 }
