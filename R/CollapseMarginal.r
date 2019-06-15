e1e204f9a22b073da91e5bc296df3dcb17ac9763
Tuomisto Jouni <jouni.tuomisto@thl.fi>
2019-06-15 12:22:47 +0300

    CollapseMarginal debugged. Now sample requires one index per row, other functions may have several

--- R/CollapseMarginal.r
+++ R/CollapseMarginal.r
@@ -15,7 +15,7 @@ CollapseTableParser <- function (CTable, env = .GlobalEnv) # There is a bug in c
 { 
   for (i in unique(as.character(CTable$Variable))) {
     temp <- CTable[CTable$Variable == i, ]
-    cols <- strsplit(as.character(temp[["Index"]]), ",")#[[1]] # Assumes one row per ovariable
+    cols <- strsplit(as.character(temp[["Index"]]), ",") # Assumes one row per index for sample and one row per ovariable for other functions
     cols <- lapply(cols, trimws)
     probs <- strsplit(as.character(temp[["Probs"]]), ",") 
     probs <- lapply(probs, as.numeric) 
@@ -35,12 +35,13 @@ CheckCollapse <- function(variable, indent = 0, verbose = TRUE, ...) {
 	return(variable)
 }
 
-CollapseMarginal <- function(variable, cols, fun = "mean", probs = NULL, ...) { # cols and probs are lists
+CollapseMarginal <- function(variable, cols, fun = "mean", probs = NULL, ...) { # cols and probs are lists. If cols is not a list, it is converted to a list of leangth 1.
   if (length(fun) == 0) fun <- "mean"
   if (is.na(fun)) stop("No function given to collapse with!\n")
   if(length(fun)==1) fun <- rep(fun,length(cols)) # Make sure that function works if fun is omitted.
   if(is.null(probs)) probs <- rep(list(NA),length(cols)) # Make sure probs is a list with same length as cols
   probs[sapply(probs,length)==0] <- NA # Make sure empty probs are NA
+  if(!is.list(cols)) cols <- list(cols)
   
 	# If no probabilities given use function
 	# Also if given function is sample then equal weights are used and this section is skipped
@@ -51,7 +52,7 @@ CollapseMarginal <- function(variable, cols, fun = "mean", probs = NULL, ...) { 
   out <- variable@output
   marginals <- colnames(out)[variable@marginal]
   for(i in 1:length(cols)) {
-  	if (is.na(probs[[i]]) & fun[[i]] != "sample") {
+  	if (is.na(probs[[i]][1]) & fun[[i]] != "sample") {
   		out <- oapply(variable, FUN = fun[[i]], cols = cols[[i]], na.rm = TRUE)
   		return(out)
   	}
@@ -65,15 +66,15 @@ CollapseMarginal <- function(variable, cols, fun = "mean", probs = NULL, ...) { 
   	} else {
   		N <- get("N", openv)
   	}
-#  	for (i in 1:length(cols)) {
+#  	for (i in 1:length(cols)) { # Not needed because with sample, length(cols[[i]]) is always 1 (if not, others are ignored)
   		b <- probs[[i]]
-  		locs <- sort(unique(out[[cols[i]]])) # This is sorted so that the order is always known.
+  		locs <- sort(unique(out[[cols[[i]][1]]])) # This is sorted so that the order is always known.
   		if (is.null(b)) b <- rep(1, length(locs)) # dont see why NA would turn up here, but hey lets just be sure
   		if (any(is.na(b))) b <- rep(1, length(locs))
   		if (length(b) != length(locs)) {
   			stop(paste("Number of locations does not match number of probabilities given for ", cols[i], "!\n", sep = ""))
   		}
-  		selection <- data.frame(
+  		selection <- data.frame( # This does NOT follow oapply logic, i.e. ignores all other marginals than Iter.
   			Iter = 1:N, 
   			sample(
   				locs, 
